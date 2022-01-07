476fd48c4f39aa07e6853d2f457da0f51c0875bb
vilevent <96803412+vilevent@users.noreply.github.com>
2022-01-07 18:56:20 -0500

    Update SQL_MoreTopics.sql

--- SQL_MoreTopics.sql
+++ SQL_MoreTopics.sql
@@ -8,11 +8,11 @@ USE employees;
 	-- assign '110022' as 'manager' to all individuals whose employee number is lower than or equal to 10020, 
 	-- and '110039' to those whose number is between 10021 and 10040 inclusive. 
 SELECT 
-	e.emp_no, 
-	MIN(de.dept_no) AS dept_no,
-    IF(e.emp_no <= 10020, 110022, 110039) AS manager
+   e.emp_no, 
+   MIN(de.dept_no) AS dept_no,
+   IF(e.emp_no <= 10020, 110022, 110039) AS manager
 FROM employees e JOIN dept_emp de
-	ON e.emp_no = de.emp_no
+   ON e.emp_no = de.emp_no
 WHERE e.emp_no <= 10040
 GROUP BY e.emp_no;
 
@@ -26,10 +26,10 @@ DROP PROCEDURE IF EXISTS get_emp_info_dept;
 DELIMITER $$
 
 CREATE PROCEDURE get_emp_info_dept (
-	IN p_emp_no INT
+    IN p_emp_no INT
 )
 BEGIN
-	DECLARE latest_from_date DATE;
+    DECLARE latest_from_date DATE;
     
     SELECT MAX(from_date)
     INTO latest_from_date
@@ -38,10 +38,10 @@ BEGIN
     
     SELECT e.emp_no, d.dept_no, d.dept_name
     FROM employees e JOIN dept_emp de
-		ON e.emp_no = de.emp_no
-	JOIN departments d 
-		ON de.dept_no = d.dept_no
-	WHERE e.emp_no = p_emp_no AND de.from_date = latest_from_date;
+    	ON e.emp_no = de.emp_no
+    JOIN departments d 
+    	ON de.dept_no = d.dept_no
+    WHERE e.emp_no = p_emp_no AND de.from_date = latest_from_date;
     
 END $$
 
@@ -79,13 +79,13 @@ CREATE TRIGGER before_insert_emp_hire_date
 BEFORE INSERT ON employees
 FOR EACH ROW
 BEGIN
-	DECLARE today DATE;
+    DECLARE today DATE;
     
     SET today = CURDATE();
     
-	IF NEW.hire_date > today THEN
-		SET NEW.hire_date = today;
-	END IF;
+    IF NEW.hire_date > today THEN
+    	SET NEW.hire_date = today;
+    END IF;
 END $$
 
 DELIMITER ;
@@ -113,31 +113,31 @@ DROP FUNCTION IF EXISTS get_emp_salary_info;
 DELIMITER $$
 
 CREATE FUNCTION get_emp_salary_info (
-	p_emp_no INT,
+    p_emp_no INT,
     char_seq VARCHAR(25)
 )
 RETURNS INT
 DETERMINISTIC
 BEGIN
 
-	DECLARE salary_calc INT;
+    DECLARE salary_calc INT;
     
     IF char_seq = 'min' THEN
-		(SELECT MIN(salary)
-		INTO salary_calc
-		FROM salaries
-		WHERE emp_no = p_emp_no);
-	ELSEIF char_seq = 'max' THEN
-		(SELECT MAX(salary)
-		INTO salary_calc
-		FROM salaries
-		WHERE emp_no = p_emp_no);
-	ELSE
-		(SELECT MAX(salary) - MIN(salary)
-		INTO salary_calc
-		FROM salaries
-		WHERE emp_no = p_emp_no);
-	END IF;
+	(SELECT MIN(salary)
+	INTO salary_calc
+	FROM salaries
+	WHERE emp_no = p_emp_no);
+    ELSEIF char_seq = 'max' THEN
+	(SELECT MAX(salary)
+	INTO salary_calc
+	FROM salaries
+	WHERE emp_no = p_emp_no);
+    ELSE
+	(SELECT MAX(salary) - MIN(salary)
+	INTO salary_calc
+	FROM salaries
+	WHERE emp_no = p_emp_no);
+    END IF;
 
 RETURN salary_calc;	
     
@@ -148,13 +148,13 @@ DELIMITER ;
 -- Apply this function to employee number 11356, for all three scenarios. 
 -- Then, apply this function to all employees, finding the lowest and highest salary.
 SELECT 
-	get_emp_salary_info(11356, 'min') AS min_salary,
+    get_emp_salary_info(11356, 'min') AS min_salary,
     get_emp_salary_info(11356, 'max') AS max_salary;
     
 SELECT get_emp_salary_info(11356, 'what to write') AS salary_diff;
 
 SELECT 
-	emp_no, 
+    emp_no, 
     get_emp_salary_info(emp_no, 'min') AS min_salary, 
     get_emp_salary_info(emp_no, 'max') AS max_salary
 FROM employees;
@@ -195,23 +195,23 @@ WHERE last_name IN ('Farris', 'Kulisch', 'Speckmann', 'Munoz');
 -- Combine the information (employee number, name, salary, from date, to date) for employees in the salary ranges 
 -- [45000, 60000] and [90000, 110000] into a single result set, using UNION or UNION ALL. Try to optimize the performance.
 SELECT 
-	e.emp_no,
-	CONCAT(e.first_name, ' ', e.last_name) AS full_name,
+    e.emp_no,
+    CONCAT(e.first_name, ' ', e.last_name) AS full_name,
     s.salary,
     s.from_date, 
     s.to_date
 FROM employees e JOIN salaries s
-	ON e.emp_no = s.emp_no
+    ON e.emp_no = s.emp_no
 WHERE s.salary BETWEEN 45000 AND 60000
 UNION ALL
 SELECT 
-	e.emp_no,
-	CONCAT(e.first_name, ' ', e.last_name) AS full_name,
+    e.emp_no,
+    CONCAT(e.first_name, ' ', e.last_name) AS full_name,
     s.salary,
     s.from_date, 
     s.to_date
 FROM employees e JOIN salaries s
-	ON e.emp_no = s.emp_no
+    ON e.emp_no = s.emp_no
 WHERE s.salary BETWEEN 90000 AND 110000;
 
 
@@ -220,19 +220,19 @@ WHERE s.salary BETWEEN 90000 AND 110000;
 -- the maximum and minimum salary of that employee, and another one saying whether this salary 
 -- raise was higher than $30,000, in between $20,000 and $30,000, or lower than $20,000.
 SELECT 
-	dm.emp_no,
+    dm.emp_no,
     e.first_name,
     e.last_name,
     MAX(s.salary) - MIN(s.salary) AS salary_raise,
     CASE
-		WHEN MAX(s.salary) - MIN(s.salary) > 30000 THEN 'Salary raise was higher than $30,000'
+	WHEN MAX(s.salary) - MIN(s.salary) > 30000 THEN 'Salary raise was higher than $30,000'
         WHEN MAX(s.salary) - MIN(s.salary) BETWEEN 20000 AND 30000 THEN 'Salary raise was between $20,000 and $30,000'
-		ELSE 'Salary raise was lower than $20,000'
-	END AS salary_raise_desc
+	ELSE 'Salary raise was lower than $20,000'
+    END AS salary_raise_desc
 FROM employees e JOIN dept_manager dm
-	ON e.emp_no = dm.emp_no
+    ON e.emp_no = dm.emp_no
 JOIN salaries s
-	ON dm.emp_no = s.emp_no
+    ON dm.emp_no = s.emp_no
 GROUP BY dm.emp_no, e.first_name, e.last_name;
 
 
@@ -241,12 +241,12 @@ GROUP BY dm.emp_no, e.first_name, e.last_name;
 -- employee is still working in the company, or 'Not an employee anymore' if they aren’t.
 -- To date of '9999-01-01' signifies that the employee is still working for the company.
 SELECT
-	e.emp_no,
+    e.emp_no,
     e.first_name,
     e.last_name,
     IF(MAX(de.to_date) = '9999-01-01', 'Is still employed', 'Not an employee anymore') AS emp_status
 FROM employees e JOIN dept_emp de
-	ON e.emp_no = de.emp_no
+    ON e.emp_no = de.emp_no
 WHERE e.emp_no <= 11000
 GROUP BY e.emp_no;
 
